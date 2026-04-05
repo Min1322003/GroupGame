ec791a29205ae727cbc48d2bddfd9116061b6e67
Min1322003 <mmaw@oswego.edu>
2026-04-05 17:30:08 -0400

    Fresh 2D Unity project

---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    Vector2 movement;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
